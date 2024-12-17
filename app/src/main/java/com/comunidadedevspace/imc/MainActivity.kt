ed74d374e0de6fd3f72894dd004e85c887d26c61
otavi <otaviogandhi@gmail.com>
2024-12-17 20:22:16 -0300

    Tela inicial Edittext

--- app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
+++ app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
@@ -2,10 +2,26 @@ package com.comunidadedevspace.imc
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        val edtPeso = findViewById<TextInputEditText>(R.id.edittext_peso);
+        val edtAltura = findViewById<TextInputEditText>(R.id.edittext_altura);
+        val btnCalcular = findViewById<Button>(R.id.btn_calcular);
+
+            btnCalcular.setOnClickListener{
+                val peso = edtPeso.text;// esse .text é usado para guardar a informação digitada no peso
+                val altura = edtAltura.text // usado para guardar a info da altura
+                println("OTAVIO sua altura é de :"+ altura);
+
+
+
+            }
     }
 }
