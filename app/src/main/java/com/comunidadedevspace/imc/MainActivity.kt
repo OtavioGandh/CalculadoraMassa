c68b98b14fe6d9b95b2058d636d0c20e3d793b42
otavi <otaviogandhi@gmail.com>
2024-12-18 19:28:26 -0300

    Mostrar mensagem preencha os campos

--- app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
+++ app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
@@ -3,6 +3,7 @@ package com.comunidadedevspace.imc
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
@@ -11,18 +12,34 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
 
-        val edtPeso = findViewById<TextInputEditText>(R.id.edittext_peso);
-        val edtAltura = findViewById<TextInputEditText>(R.id.edittext_altura);
+        val edtPeso = findViewById<TextInputEditText>(R.id.edittext_peso)
+        val edtAltura = findViewById<TextInputEditText>(R.id.edittext_altura)
+
         val btnCalcular = findViewById<Button>(R.id.btn_calcular)
 
-            btnCalcular.setOnClickListener{
-                val peso: Float = edtPeso.text.toString().toFloat();// esse .text é usado para guardar a informação digitada no peso
-                val altura: Float = edtAltura.text.toString().toFloat() // usado para guardar a info da altura
+        btnCalcular.setOnClickListener{
+                val pesoStr: String = edtPeso.text.toString()
+                val alturaStr: String = edtAltura.text.toString()
 
-                val alturaQ2 = altura * altura;
-                val resultado = peso / alturaQ2;
-                println("Rodano o codigo" + resultado)
+                if(pesoStr == "" || alturaStr == ""){
+                        Snackbar.make(
+                                edtPeso,
+                                "Preencha todos os campos",
+                                Snackbar.LENGTH_LONG,
+                            )
+                            .show()
+                } else{
+                        val peso = pesoStr.toFloat()
+                        val altura = alturaStr.toFloat()
 
-            }
+                        val alturaQ2 = altura * altura
+                        val resultado = peso / alturaQ2
+
+                        println("Rodano o codigo" + resultado)
+                }
+
+
+
+           }
     }
 }
